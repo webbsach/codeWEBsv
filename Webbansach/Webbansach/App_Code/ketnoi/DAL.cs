01836329bd9096930e48525c0a171eaa874a0e1b
nanhtrang1996 <nanhtrang1996@gmail.com>
2017-12-26 22:55:45 +0700

    Ketnoi

---
+++ Webbansach/Webbansach/App_Code/ketnoi/DAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web;
+
+/// <summary>
+/// Summary description for DAL
+/// </summary>
+public static class DAL
+{
+public static void ketnoi()
+    {
+        string cnt = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename="; 
+        string cnl= ";Integrated Security=True;Connect Timeout=30";
+        SqlDataReader reader = null;
+        SqlConnection conn = null;
+        // string cs = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+        string a = System.IO.Directory.GetCurrentDirectory();
+        a.Substring(0, a.Length - 9);
+        a += "Data\\WebBanSach.mdf";
+        string cs = cnt + a + cnl;
+        conn = new SqlConnection(cs);
+        conn.Open();
+
+    }
+}
