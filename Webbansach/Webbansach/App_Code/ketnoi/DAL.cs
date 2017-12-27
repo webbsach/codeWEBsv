d7113e2f36a459166204ff3ae443481df2bac211
phuongthuy01896 <phuongthuy01896@gmail.com>
2017-12-27 20:47:32 +0700

    hoan thanh service

--- Webbansach/Webbansach/App_Code/ketnoi/DAL.cs
+++ Webbansach/Webbansach/App_Code/ketnoi/DAL.cs
@@ -15,13 +15,13 @@ public static SqlConnection ketnoi()
         string cnl= ";Integrated Security=True;Connect Timeout=30";
         SqlDataReader reader = null;
         SqlConnection conn = null;
-        // string cs = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+        string cs = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
         string a = System.IO.Directory.GetCurrentDirectory();
         a.Substring(0, a.Length - 9);
         a += "Data\\WebBanSach.mdf";
-        string cs = cnt + a + cnl;
+      //  string cs = cnt + a + cnl;
         conn = new SqlConnection(cs);
-        conn.Open();
+   
         return conn;
     }
 }
